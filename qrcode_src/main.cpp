51c2f9386f4cfa54a3eceb15bde75776d082cc47
n3idhardt <icefooooooooox@gmail.com>
2018-12-19 17:24:30 -0800

    modify value of qrcode

--- qrcode_src/main.cpp
+++ qrcode_src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
 	string blkRst;
 	int errcode = QR_ERR_NONE;
 	/* argv[1]为mac地址，argv[2]为拍摄时间 */
-	blkRst.append(argv[1]).append(" ").append(argv[2]);
+	blkRst.append("machineid=").append(argv[1]).append("&").append("date=").append(argv[2]);
 	QRCode* p = qrInit(10, QR_EM_8BIT, 2, -1, &errcode);
 	if (p == NULL) {
 		printf("error\n");
