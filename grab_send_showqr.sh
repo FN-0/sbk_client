6193dc3db0cc77f191ec1ae9beeeed0a9baed4ce
Neidhardt <icefooooooooox@gmail.com>
2019-01-08 15:27:05 +0800

    upload 5 imgs

--- grab_send_showqr.sh
+++ grab_send_showqr.sh
@@ -3,11 +3,23 @@
 # !!!修改时请注意使用'LF换行'!!!
 
 # 获取当前时间
-datetime=$(date +%Y%m%d%H%M%S)
+datetime1=$(date +%Y%m%d%H%M%S)
+sleep 1
+datetime2=$(date +%Y%m%d%H%M%S)
+sleep 1
+datetime3=$(date +%Y%m%d%H%M%S)
+sleep 1
+datetime4=$(date +%Y%m%d%H%M%S)
+sleep 1
+datetime5=$(date +%Y%m%d%H%M%S)
 # 获取mac地址并去掉冒号
 mac_addr="`cat /sys/class/net/wlan0/address | sed 's/://g'`"
 # 图片名为mac地址+该程序运行时间
-image_name=${mac_addr}${datetime}.jpg
+image_name1=${mac_addr}${datetime1}.jpg
+image_name2=${mac_addr}${datetime2}.jpg
+image_name3=${mac_addr}${datetime3}.jpg
+image_name4=${mac_addr}${datetime4}.jpg
+image_name5=${mac_addr}${datetime5}.jpg
 # 客户端存放位置
 clientpath="/home/pi/sbk_client/"
 
@@ -30,25 +42,41 @@ fi
 
 # 拍摄图片
 # https://github.com/twam/v4l2grab
-./v4l2grab -d/dev/video0 -W1920 -H1080  -q100 -m -o${image_name}
+./v4l2grab -d/dev/video0 -W1920 -H1080  -q100 -m -o${image_name1}
+sleep 1
+./v4l2grab -d/dev/video0 -W1920 -H1080  -q100 -m -o${image_name2}
+sleep 1
+./v4l2grab -d/dev/video0 -W1920 -H1080  -q100 -m -o${image_name3}
+sleep 1
+./v4l2grab -d/dev/video0 -W1920 -H1080  -q100 -m -o${image_name4}
+sleep 1
+./v4l2grab -d/dev/video0 -W1920 -H1080  -q100 -m -o${image_name5}
 
 # upload & show qr code & upload fail process
 # 上传图片
 # 如果上传失败显示含有上传失败文字的二维码
 # 如果上传成功显示含有url的二维码，并且重试之前上传失败的图片
-if [ ! -f ${image_name} ]; then
+if [ ! -f ${image_name1} ]; then
 	echo "Image does not exist."
 	exit 0
 fi
-mv ${image_name} images/
+mv ${image_name1} images/
+mv ${image_name2} images/
+mv ${image_name3} images/
+mv ${image_name4} images/
+mv ${image_name5} images/
 cd images/
-sleep 10 # 经实际测试，上传时可能还没连上wifi需要加延时
+# sleep 10 # 经实际测试，上传时可能还没连上wifi需要加延时
 echo "Start uploading."
-../upload_img ${image_name}
+../upload_img ${image_name1}
+../upload_img ${image_name2}
+../upload_img ${image_name3}
+../upload_img ${image_name4}
+../upload_img ${image_name5}
 ret=$?
 # 使用程序返回值作为上传成功或失败的依据
 if [ ${ret} -eq 2 ]; then
-	cp ${image_name} ../upload_failed/
+	#cp ${image_name} ../upload_failed/
 	cd ..
 	echo "upload failed"
 	# show "upload failed" with qr code
@@ -63,21 +91,21 @@ elif [ ${ret} -eq 0 ]; then
 	echo "qrcode"
 	cd ..
 	# display qr code
-	./qrcode ${mac_addr} ${datetime}
+	./qrcode ${mac_addr} ${datetime1} ${mac_addr} ${datetime2} ${mac_addr} ${datetime3} ${mac_addr} ${datetime4} ${mac_addr} ${datetime5}
 	feh -Y -F -m -H 480 -W 800 --bg bg.png -a 0 -E 470 -y 470 qr.bmp &
 	# re-upload
 	# 重传部分，循环上传并且删除（如果成功）
-	reupload_images=`ls upload_failed/`
-	if [ ! `ls upload_failed/ $*|wc -w` -eq 0 ]; then 
-		for reup_img in ${reupload_images}; do
-			# re-upload them!
-			./upload_img ${reup_img}
-			reupret=$?
-			if [ ${reupret} -eq 0 ]; then
-				rm ${reup_img}
-			fi
-  		done
-	fi
+	#reupload_images=`ls upload_failed/`
+	#if [ ! `ls upload_failed/ $*|wc -w` -eq 0 ]; then 
+	#	for reup_img in ${reupload_images}; do
+	#		# re-upload them!
+	#		./upload_img ${reup_img}
+	#		reupret=$?
+	#		if [ ${reupret} -eq 0 ]; then
+	#			rm ${reup_img}
+	#		fi
+  	#	done
+	#fi
 	# shutdown in few minutes
 	sleep 300
 	shutdown now
