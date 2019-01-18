df5ae4f47758d8bff460e6d697bc2e5b4919733c
Neidhardt <icefooooooooox@gmail.com>
2019-01-18 10:42:03 +0800

    190118

--- upload_img.c
+++ upload_img.c
@@ -1,207 +1,70 @@
-/***************************************************************************
- *                                  _   _ ____  _
- *  Project                     ___| | | |  _ \| |
- *                             / __| | | | |_) | |
- *                            | (__| |_| |  _ <| |___
- *                             \___|\___/|_| \_\_____|
- *
- * Copyright (C) 1998 - 2017, Daniel Stenberg, <daniel@haxx.se>, et al.
- *
- * This software is licensed as described in the file COPYING, which
- * you should have received as part of this distribution. The terms
- * are also available at https://curl.haxx.se/docs/copyright.html.
- *
- * You may opt to use, copy, modify, merge, publish, distribute and/or sell
- * copies of the Software, and permit persons to whom the Software is
- * furnished to do so, under the terms of the COPYING file.
- *
- * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
- * KIND, either express or implied.
- *
- ***************************************************************************/
-/* <DESC>
- * using the multi interface to do a multipart formpost without blocking
- * </DESC>
- */
-
 #include <stdio.h>
 #include <string.h>
-#include <sys/time.h>
-
+ 
 #include <curl/curl.h>
-
-int main(int argc, char ** argv)
+ 
+int main(int argc, char *argv[])
 {
   CURL *curl;
+  CURLcode res;
+ 
+  int ret;
+  //char *url;
 
-  CURLM *multi_handle;
-  int still_running;
-
-  curl_mime *form = NULL;
-  curl_mimepart *field = NULL;
-  struct curl_slist *headerlist = NULL;
+  struct curl_httppost *formpost=NULL;
+  struct curl_httppost *lastptr=NULL;
+  struct curl_slist *headerlist=NULL;
   static const char buf[] = "Expect:";
+ 
+  //sprintf(url, "http://sup-heal.com:9080/picture");
 
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  curl_formadd(&formpost,
+               &lastptr,
+               CURLFORM_COPYNAME, "filename",
+               CURLFORM_COPYCONTENTS, argv[1],
+               CURLFORM_END);
+  
+  /* Fill in the submit field too, even if this is rarely needed */  
+  curl_formadd(&formpost,
+               &lastptr,
+               CURLFORM_COPYNAME, "picture",
+               CURLFORM_FILE, argv[1],
+               CURLFORM_CONTENTTYPE, "image/jpeg",
+               CURLFORM_END);
+ 
   curl = curl_easy_init();
-  multi_handle = curl_multi_init();
-
-  if(curl && multi_handle) {
-    /* Create the form */
-    form = curl_mime_init(curl);
-
-    /* Fill in the file upload field */
-    field = curl_mime_addpart(form);
-    curl_mime_name(field, "picture");
-    curl_mime_filedata(field, argv[1]);
-
-    /* Fill in the filename field */
-    field = curl_mime_addpart(form);
-    curl_mime_name(field, "filename");
-    curl_mime_data(field, argv[1], CURL_ZERO_TERMINATED);
-    
-    /* Fill in the file upload field */
-    field = curl_mime_addpart(form);
-    curl_mime_name(field, "picture1");
-    curl_mime_filedata(field, argv[2]);
-
-    /* Fill in the filename field */
-    field = curl_mime_addpart(form);
-    curl_mime_name(field, "filename1");
-    curl_mime_data(field, argv[2], CURL_ZERO_TERMINATED);
-
-    /* Fill in the file upload field */
-    field = curl_mime_addpart(form);
-    curl_mime_name(field, "picture2");
-    curl_mime_filedata(field, argv[3]);
-
-    /* Fill in the filename field */
-    field = curl_mime_addpart(form);
-    curl_mime_name(field, "filename2");
-    curl_mime_data(field, argv[3], CURL_ZERO_TERMINATED);
-
-    /* Fill in the file upload field */
-    field = curl_mime_addpart(form);
-    curl_mime_name(field, "picture3");
-    curl_mime_filedata(field, argv[4]);
-
-    /* Fill in the filename field */
-    field = curl_mime_addpart(form);
-    curl_mime_name(field, "filename3");
-    curl_mime_data(field, argv[4], CURL_ZERO_TERMINATED);
-
-    /* Fill in the file upload field */
-    field = curl_mime_addpart(form);
-    curl_mime_name(field, "picture4");
-    curl_mime_filedata(field, argv[5]);
-
-    /* Fill in the filename field */
-    field = curl_mime_addpart(form);
-    curl_mime_name(field, "filename4");
-    curl_mime_data(field, argv[5], CURL_ZERO_TERMINATED);
-
-    /* Fill in the submit field too, even if this is rarely needed */
-    //field = curl_mime_addpart(form);
-    //curl_mime_name(field, "submit");
-    //curl_mime_data(field, "send", CURL_ZERO_TERMINATED);
-
-    /* initialize custom header list (stating that Expect: 100-continue is not
-       wanted */
-    headerlist = curl_slist_append(headerlist, buf);
-
-    /* what URL that receives this POST */
-    curl_easy_setopt(curl, CURLOPT_URL, "http://www.sup-heal.com:9080/picture/FiveimageUpload");
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
-    curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
-
-    curl_multi_add_handle(multi_handle, curl);
-
-    curl_multi_perform(multi_handle, &still_running);
-
-    do {
-      struct timeval timeout;
-      int rc; /* select() return code */
-      CURLMcode mc; /* curl_multi_fdset() return code */
-
-      fd_set fdread;
-      fd_set fdwrite;
-      fd_set fdexcep;
-      int maxfd = -1;
-
-      long curl_timeo = -1;
-
-      FD_ZERO(&fdread);
-      FD_ZERO(&fdwrite);
-      FD_ZERO(&fdexcep);
-
-      /* set a suitable timeout to play around with */
-      timeout.tv_sec = 1;
-      timeout.tv_usec = 0;
-
-      curl_multi_timeout(multi_handle, &curl_timeo);
-      if(curl_timeo >= 0) {
-        timeout.tv_sec = curl_timeo / 1000;
-        if(timeout.tv_sec > 1)
-          timeout.tv_sec = 1;
-        else
-          timeout.tv_usec = (curl_timeo % 1000) * 1000;
-      }
-
-      /* get file descriptors from the transfers */
-      mc = curl_multi_fdset(multi_handle, &fdread, &fdwrite, &fdexcep, &maxfd);
-
-      if(mc != CURLM_OK) {
-        fprintf(stderr, "curl_multi_fdset() failed, code %d.\n", mc);
-        break;
-      }
-
-      /* On success the value of maxfd is guaranteed to be >= -1. We call
-         select(maxfd + 1, ...); specially in case of (maxfd == -1) there are
-         no fds ready yet so we call select(0, ...) --or Sleep() on Windows--
-         to sleep 100ms, which is the minimum suggested value in the
-         curl_multi_fdset() doc. */
-
-      if(maxfd == -1) {
-#ifdef _WIN32
-        Sleep(100);
-        rc = 0;
-#else
-        /* Portable sleep for platforms other than Windows. */
-        struct timeval wait = { 0, 100 * 1000 }; /* 100ms */
-        rc = select(0, NULL, NULL, NULL, &wait);
-#endif
-      }
-      else {
-        /* Note that on some platforms 'timeout' may be modified by select().
-           If you need access to the original value save a copy beforehand. */
-        rc = select(maxfd + 1, &fdread, &fdwrite, &fdexcep, &timeout);
-      }
-
-      switch(rc) {
-      case -1:
-        /* select error */
-        break;
-      case 0:
-      default:
-        /* timeout or readable/writable sockets */
-        printf("perform!\n");
-        curl_multi_perform(multi_handle, &still_running);
-        printf("running: %d!\n", still_running);
-        break;
-      }
-    } while(still_running);
-
-    curl_multi_cleanup(multi_handle);
-
-    /* always cleanup */
+  /* initalize custom header list (stating that Expect: 100-continue is not
+     wanted */ 
+  headerlist = curl_slist_append(headerlist, buf);
+  if(curl) {
+    /* what URL that receives this POST */ 
+    curl_easy_setopt(curl, CURLOPT_URL, "http://www.sup-heal.com:9080/picture/imageUpload");
+    if ( (argc == 3) && (!strcmp(argv[2], "noexpectheader")) )
+      /* only disable 100-continue header if explicitly requested */ 
+      curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
+    curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
+    /* abort if slower than 8192 bytes/sec during 30 seconds */
+    curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, 30L);
+    curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 8192L);
+    /* Perform the request, res will get the return code */ 
+    res = curl_easy_perform(curl);
+    /* Check for errors */ 
+    if(res != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+      ret = 2;
+    }
+    /* always cleanup */ 
     curl_easy_cleanup(curl);
 
-    /* then cleanup the form */
-    curl_mime_free(form);
-
-    /* free slist */
-    curl_slist_free_all(headerlist);
+    /* then cleanup the formpost chain */ 
+    curl_formfree(formpost);
+    /* free slist */ 
+    curl_slist_free_all (headerlist);
   }
+  if(ret == 2)
+    return 2;
   return 0;
 }
