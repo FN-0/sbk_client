5d6a4d2fea53d1b5c7a3116af6b86585b3cea0b4
Neidhardt <icefooooooooox@gmail.com>
2019-06-20 15:59:42 +0800

    !

--- grab_send_showqr.sh
+++ grab_send_showqr.sh
@@ -90,16 +90,16 @@ if [ ! -f ${image_name1} ]; then
 	echo "Image does not exist."
 	notify-send 图片未拍摄成功
 	exit 0
-elif [ -f images/${image_name2} ]; then
+elif [ ! -f images/${image_name2} ]; then
 	notify-send 图片未拍摄成功
 	exit 0
-elif [ -f images/${image_name3} ]; then
+elif [ ! -f images/${image_name3} ]; then
 	notify-send 图片未拍摄成功
 	exit 0
-elif [ -f images/${image_name4} ]; then
+elif [ ! -f images/${image_name4} ]; then
 	notify-send 图片未拍摄成功
 	exit 0
-elif [ -f images/${image_name5} ]; then
+elif [ ! -f images/${image_name5} ]; then
 	notify-send 图片未拍摄成功
 	exit 0
 fi
