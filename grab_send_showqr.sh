96f7df1efab923e5c32fff0f7d50ab50255267af
Neidhardt <icefooooooooox@gmail.com>
2019-10-12 10:15:01 +0800

    er

--- grab_send_showqr.sh
+++ grab_send_showqr.sh
@@ -2,7 +2,7 @@
 
 # 请注意使用'LF换行'
 
-python /home/pi/sbk_client/motor_controller.py 11 12 6 7
+#python /home/pi/sbk_client/motor_controller.py 11 12 6 7
 
 ping -c 1 121.40.169.248 > /dev/null 2>&1
 while [ $? -ne 0 ]; do
@@ -22,7 +22,7 @@ mac_addr="`cat /sys/class/net/wlan0/address | sed 's/://g'`"
 image_name1=${mac_addr}${datetime1}.jpg
 # 客户端存放位置
 clientpath="/home/pi/sbk_client/"
-
+im_path="/home/pi/sbk_client/example.jpg"
 unclutter -idle 0.01 -root &
 
 # 检查摄像头是否存在
@@ -46,10 +46,24 @@ if [ ! -d "./images" ]; then
 fi
 
 notify-send  正在拍摄
+sleep 3
 
-python /home/pi/sbk_client/motor_controller.py 15 16 3 3 &
-
+#python /home/pi/sbk_client/motor_controller.py 15 16 3 3 &
+pos_data=
 notify-send  正在上传
-res=`curl --max-time 180 -F "picture=@/home/pi/sbk_client/images/${image_name1}" -F "rgb=${pos_data}"  http://121.40.169.248:9080/picture/python/pythonUploadAndAnalysis`
-
+res=`curl --max-time 180 -F "picture=@/home/pi/sbk_client/example.jpg" -F "rgb=${pos_data}"  http://121.40.169.248:9080/picture/python/pythonUploadAndAnalysis`
+if [[ "${res}" == "" ]]; then
+	cd ..
+	echo "upload failed"
+	qrencode -s 6 -o qr.bmp "上传失败"
+	# feh 显示二维码
+	feh -Y -F -m -H 480 -W 800 --bg bg.png -a 0 -E 470 -y 470 qr.bmp &
+	# 5分钟后自动关机
+	notify-send -t 0 网络信号不佳
+elif [[ "${res}" != "" ]]; then
+	echo "qrcode"
+	cd ..
+	qrencode -s 4 -o qr.bmp "http://userclient.fun-med.cn/health?heal=${res}"
+	feh -Y -x -m -H 480 -W 800 --bg bg.png -a 0 -E 470 -y 470 qr.bmp &
+fi
 exit 0
