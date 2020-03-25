05bdd5f09fef922e79ddf880e5a07cf1c3ffc8e3
Neidhardt <icefooooooooox@gmail.com>
2020-03-25 15:10:40 +0800

    1

--- test.sh
+++ test.sh
@@ -25,7 +25,7 @@ python /home/pi/sbk_client/algorithm_blo_detection_new.py /home/pi/sbk_client/te
 
 filename="/home/pi/sbk_client/block_pos.txt"
 pos_data=`head -n 1 ${filename}`
-res=`curl --max-time 180 -F "picture=@/home/pi/sbk_client/test/test.jpg" -F "coordinates=${pos_data}"  http://deviceapi.fun-med.cn/device/v2/upload/fluid/14items`
+res=`curl --max-time 180 -F "picture=@/home/pi/sbk_client/test/b827ebd33d8920200323140246.jpg" -F "coordinates=${pos_data}"  http://deviceapi.fun-med.cn/device/v2/upload/fluid/14items`
 
 filename="/home/pi/sbk_client/res.txt"
 res=$(cat ${filename})
